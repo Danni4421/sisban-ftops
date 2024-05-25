0e226259fbf3908ac33a83b24d6069b6ca2032d7
Danni4421 <endaelek1234@gmail.com>
2024-05-25 08:33:56 +0700

    feat: add fuzzy method

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,13 +1,25 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify, render_template
+from .rules import init_rules
+from libs.fuzzy import Fuzzy
 
 main = Blueprint('main', __name__)
 
 
 @main.route('/', methods=['GET'])
 def index():
-    return 'main index'
+    return render_template('index.html')
 
 
 @main.route('/calculate', methods=['POST'])
 def calculate():
-    return 'for calculating'
+    alternatives = request.get_json().get('alternative')
+    rules = init_rules()
+
+    economy_condition = []
+
+    for alternative in alternatives:
+        fuzzy = Fuzzy(rules=rules, data={
+            'gaji': alternative.get('gaji') / 1000000,
+            'pengeluaran': alternative.get('pengeluaran') / 1000000
+        })
+        economy_condition.append(fuzzy.exec())
--- libs/fuzzy.py
+++
@@ -1,3 +0,0 @@
-class Fuzzy:
-    def __init__(self):
-        print('fuzzy')
